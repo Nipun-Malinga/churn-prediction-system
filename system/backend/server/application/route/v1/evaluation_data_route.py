b593d9b9af326ef11ea80f73d7366bed94924b9e
Nipun Malinga <nipunmalinga11@gmail.com>
2025-03-18 12:08:37 +0530

    Build the api route to add list of customer data to the system

--- system/backend/server/application/route/v1/evaluation_data_route.py
+++ system/backend/server/application/route/v1/evaluation_data_route.py
@@ -24,7 +24,24 @@ def add_data():
         ), 201
 
     except ValidationError as err:
-        return error_response_template(err.messages), 400 
+        return error_response_template(err.messages), 400
+    
+@data.route('/data-list', methods=['POST'])
+def add_data_list():
+    schema = Evaluation_Data_Schema(many=True)
+
+    try:
+        request_data = schema.load(request.json) 
+        saved_entries = [save_evaluation_data(entry) for entry in request_data]
+        
+        return response_template(
+            'success', 
+            f'{len(saved_entries)} new data entries added successfully', 
+            schema.dump(saved_entries)
+        ), 201
+
+    except ValidationError as err:
+        return error_response_template(err.messages), 400   
 
 @data.route('/data/csv', methods=['POST'])
 def add_csv_data():
