36bb7f078506991b01c66f9ac70fc3477e21b754
Nipun Malinga <nipunmalinga11@gmail.com>
2025-04-11 23:49:31 +0530

    Implement services and routes to fetch advanced model info and model history data. Modify services. Fix errors.

--- system/backend/server/application/route/v1/model_info_route.py
+++ system/backend/server/application/route/v1/model_info_route.py
@@ -1,6 +1,9 @@
+from enum import Enum, auto
+
 from flask import Blueprint, request, jsonify
 from application.service import Model_Info_Service
 from application.response import response_template, error_response_template
+from sqlalchemy.exc import NoResultFound
 
 model = Blueprint("model_bp", __name__)
 
@@ -8,8 +11,13 @@ service = Model_Info_Service()
 
 """
  Add a method to convert all data to dict in models.
-"""
-
+""" 
+class Type(Enum):
+    ACCURACY = "accuracy"
+    PRECISION = "precision"
+    RECALL = "recall"
+    F1_SCORE = "f1_score"
+    
 @model.route("/", methods=["GET"])
 def get_all_models():
     
@@ -22,14 +30,57 @@ def get_all_models():
     except Exception as ex:
         return error_response_template("Failed to Fetch Models"), 500
     
-@model.route("/get", methods=["GET"])    
-def get_model_info_by_id():
+@model.route("/info/basic", methods=["GET"])    
+def get_basic_model_info_by_id():
     
+    model_id = request.args.get("model_id") 
     try:
         return response_template(
             "success",
             "Model Information Fetched Successfully",
             service.get_basic_model_info(request.args.get("model_id"))
         ), 200
+    except NoResultFound as ex:
+        return error_response_template(f"No model info found for model_id: {model_id}"), 404
     except Exception as ex:
         return error_response_template("Failed to fetch Model Information"), 500
+    
+@model.route("/info/advanced", methods=["GET"])    
+def get_advanced_model_info_by_id():
+    
+    model_id = request.args.get("model_id")
+    try:
+        return response_template(
+            "success",
+            "Model Information Fetched Successfully",
+            service.get_advanced_model_info(model_id)
+        ), 200
+    except NoResultFound as ex:
+        return error_response_template(f"No model info found for model_id: {model_id}"), 404
+    except Exception as ex:
+        return error_response_template("Failed to fetch Model Information"), 500
+
+@model.route("/charts", methods=["GET"])   
+def model_history_chart_data():
+    try:
+        model_id = request.args.get("model_id")
+        filter_by = request.args.get("filter_type")
+
+        if not model_id:
+            return error_response_template("Missing required parameters"), 400
+
+        Type(filter_by)
+        
+        return response_template(
+            "success",
+            f"{filter_by.capitalize()} Data Fetched Successfully",
+            service.model_performance_history(model_id, filter_by)
+        )
+        
+    except ValueError as ex:
+        return error_response_template(
+            f"Invalid Filtering Value",
+        ), 400
+        
+    except Exception as ex:
+        return error_response_template("Failed to fetch chart history"), 500
--- system/backend/server/application/service/model_info_service.py
+++ system/backend/server/application/service/model_info_service.py
@@ -2,6 +2,7 @@ from application import db
 from application.model import Model, Model_Info
 from flask import abort
 from sqlalchemy import desc
+from sqlalchemy.exc import NoResultFound
 
 
 class Model_Info_Service:
@@ -30,23 +31,70 @@ class Model_Info_Service:
 
     @classmethod
     def get_basic_model_info(cls, model_id):
-        result = db.session.query(
-            Model_Info.id,
-            Model_Info.accuracy,
-            Model_Info.updated_date
+        try:
+            result = db.session.query(
+                Model_Info.id,
+                Model_Info.accuracy,
+                Model_Info.updated_date
+            ).filter(
+                Model_Info.model_id == model_id
+            ).order_by(
+                desc(Model_Info.updated_date)
+            ).limit(1).one()
+
+            model_info_id, accuracy, updated_date = result
+
+            return {
+                "model_info_id": model_info_id,
+                "accuracy": accuracy,
+                "updated_date": updated_date.isoformat() if updated_date else {}
+            } 
+        except NoResultFound as ex:
+            raise NoResultFound from ex
+        
+    def get_advanced_model_info(cls, model_id):
+        try:
+            result = db.session.query(
+                Model_Info
+            ).filter(
+                Model_Info.model_id == model_id
+            ).order_by(
+                desc(
+                    Model_Info.updated_date
+                )
+            ).limit(1).one()     
+
+            return result.to_dict() if result else {}
+        except NoResultFound as ex:
+            raise NoResultFound from ex
+        
+    """
+        Chart Data Services
+    """    
+    
+    @classmethod
+    def model_performance_history(cls, model_id, filter):
+        
+        match filter:
+            case "accuracy":
+                filter_by = Model_Info.accuracy
+            case "precision":
+                filter_by = Model_Info.precision
+            case "recall":
+                filter_by = Model_Info.recall
+            case "f1_score":
+                filter_by = Model_Info.f1_score
+        
+        results = db.session.query(
+            filter_by, Model_Info.updated_date
         ).filter(
             Model_Info.model_id == model_id
-        ).order_by(
-            desc(Model_Info.updated_date)
-        ).limit(1).all()
-
-        if not result:
-            abort(400, f"No model info found for model_id={model_id}")
-
-        model_info_id, accuracy, updated_date = result[0]
-
-        return {
-            "model_info_id": model_info_id,
-            "accuracy": accuracy,
-            "updated_date": updated_date.isoformat() if updated_date else None
-        }     
+        ).all()
+        
+        return [
+            {
+            "data": result[0],
+            "updated_date": result[1]
+            }
+            for result in results
+        ]
