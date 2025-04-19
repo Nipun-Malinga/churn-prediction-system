d00290ff7170b0eb715db96e07c712e777a47b28
Nipun Malinga <nipunmalinga11@gmail.com>
2025-04-20 02:54:36 +0530

    disable(client) disable dag run in model page

--- system/client/src/components/DagInfoTable/DagInfoTable.tsx
+++ system/client/src/components/DagInfoTable/DagInfoTable.tsx
@@ -1,12 +1,12 @@
 import useDagInfo from '@/hooks/useDagInfo';
-import useDagRun from '@/hooks/useDagRun';
+// import useDagRun from '@/hooks/useDagRun';
 import useDagToggle from '@/hooks/useDagToggle';
 import { Button, Table } from '@chakra-ui/react';
 
 const DagInfoTable = () => {
   const { data, refetch } = useDagInfo();
   const { mutate, isPending } = useDagToggle();
-  const { mutate: runDag } = useDagRun()
+  // const { mutate: runDag } = useDagRun();
 
   const handleToggleRun = (dag_id: string, is_paused: boolean) => {
     mutate(
@@ -19,22 +19,22 @@ const DagInfoTable = () => {
     );
   };
 
-  const handleDagRun = (dag_id: string) => {
-    runDag({ dag_id });
-  };
+  // const handleDagRun = (dag_id: string) => {
+  //   runDag({ dag_id });
+  // };
 
   return (
-    <Table.ScrollArea>
+    <Table.ScrollArea width={'100%'}>
       <Table.Root size='lg' borderRadius={'1rem'} overflow={'hidden'}>
         <Table.Header>
           <Table.Row>
             <Table.ColumnHeader>Dag Name</Table.ColumnHeader>
             <Table.ColumnHeader>Description</Table.ColumnHeader>
-            <Table.ColumnHeader>Status</Table.ColumnHeader>
             <Table.ColumnHeader>Last Triggered Time</Table.ColumnHeader>
             <Table.ColumnHeader>Next Dag Run</Table.ColumnHeader>
             <Table.ColumnHeader>Scheduled Interval</Table.ColumnHeader>
-            <Table.ColumnHeader textAlign='end'>Trigger Dag Run</Table.ColumnHeader>
+            <Table.ColumnHeader textAlign='center'>Status</Table.ColumnHeader>
+            {/* <Table.ColumnHeader textAlign='end'>Trigger Dag Run</Table.ColumnHeader> */}
           </Table.Row>
         </Table.Header>
         <Table.Body>
@@ -42,24 +42,24 @@ const DagInfoTable = () => {
             data?.data.dags.map((dag, key) => (
               <Table.Row key={key}>
                 <Table.Cell>{dag.dag_display_name}</Table.Cell>
-                <Table.Cell>{dag.description || 'Not Available'}</Table.Cell>
-                <Table.Cell>
+                <Table.Cell>{dag.description ?? 'Not Available'}</Table.Cell>
+                <Table.Cell>{dag.last_parsed_time}</Table.Cell>
+                <Table.Cell>{dag.next_dagrun ?? 'Not Available'}</Table.Cell>
+                <Table.Cell>{dag.schedule_interval.value}</Table.Cell>
+                <Table.Cell textAlign='end'>
                   <Button
                     width={'6.5rem'}
                     background={dag.is_paused ? 'red.500' : 'blue.400'}
                     loading={isPending}
                     onClick={() => handleToggleRun(dag.dag_id, !dag.is_paused)}
                   >
-                    {dag.is_paused ? 'PAUSED' : 'RUNNING'}
+                    {dag.is_paused ? 'PAUSED' : 'ACTIVE'}
                   </Button>
                 </Table.Cell>
-                <Table.Cell>{dag.last_parsed_time}</Table.Cell>
-                <Table.Cell>{dag.next_dagrun}</Table.Cell>
-                <Table.Cell>{dag.schedule_interval.value}</Table.Cell>
 
                 {/*Add Safety Mechanism To prevent triggering more than one dag run */}
 
-                <Table.Cell textAlign='end'>
+                {/* <Table.Cell textAlign='end'>
                   <Button
                     width={'6.5rem'}
                     background={'red.500'}
@@ -68,7 +68,7 @@ const DagInfoTable = () => {
                   >
                     {'Trigger'}
                   </Button>
-                </Table.Cell>
+                </Table.Cell> */}
               </Table.Row>
             ))}
         </Table.Body>
